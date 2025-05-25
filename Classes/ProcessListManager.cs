31d11f8afd273c17f43655f077aee0c2d3d2654c
Chris <79816938+szerveil@users.noreply.github.com>
2025-05-24 23:30:37 -0400

    ProcessListManager Class

--- Classes/ProcessListManager.cs
+++ Classes/ProcessListManager.cs
@@ -9,189 +9,188 @@ using System.Drawing;
 using System.Windows.Interop;
 using System.ComponentModel;
 using System.Management;
+using System.Net.Http;
+using System.IO;
+using System.Reflection;
 
-public class ProcessInfo
+public class ProcessInfoClass
 {
-    public int ProcessId { get; set; }
-    public string? ProcessName { get; set; }
-    public string? WindowTitle { get; set; }
-    public Icon? Icon { get; set; }
+    public int PROCESSID { get; set; }
+    public string? PROCESSNAME { get; set; }
+    public string? WINDOWTITLE { get; set; }
+    public Icon? ICON { get; set; }
 }
 
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 #pragma warning disable CS8603 // Possible null reference return.
 #pragma warning disable CS8604 // Possible null reference argument.
-public static class ProcessExtensions
-{
+public static class ProcessExtensions {
     [DllImport("Kernel32.dll")]
     private static extern uint QueryFullProcessImageName([In] IntPtr hProcess, [In] uint dwFlags, [Out] StringBuilder lpExeName, [In, Out] ref uint lpdwSize);
 
-    public static string? GetMainModuleFileName(this Process process, int buffer = 1024)
-    {
-        var fileNameBuilder = new StringBuilder(buffer);
-        uint bufferLength = (uint)fileNameBuilder.Capacity + 1;
-        return QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) != 0 ?
-            fileNameBuilder.ToString() :
-            null;
-    }
-
-    public static Icon GetIcon(this Process process)
-    {
-        try
-        {
-            string mainModuleFileName = process.GetMainModuleFileName();
-            return Icon.ExtractAssociatedIcon(mainModuleFileName);
-        }
-        catch
-        {
+    public static string? GetMainModuleFileName(this Process PROCESS, int BUFFER = 1024) {
+        try {
+            var FILENAMEBUILDER = new StringBuilder(BUFFER);
+            uint BUFFERLENGTH = (uint)FILENAMEBUILDER.Capacity + 1;
+            return (QueryFullProcessImageName(PROCESS.Handle, 0, FILENAMEBUILDER, ref BUFFERLENGTH) is not 0) ? FILENAMEBUILDER.ToString() : null;
+        } catch (Win32Exception) {
             return null;
         }
     }
 
-    public static Process GetParentProcess(this Process process)
-    {
-        try
-        {
-            int parentPid = 0;
-            int processPid = process.Id;
-            using (ManagementObject mo = new ManagementObject($"win32_process.handle='{processPid}'"))
-            {
-                mo.Get();
-                parentPid = Convert.ToInt32(mo["ParentProcessId"]);
+    public static Icon GETICON(this Process PROCESS) {
+        try {
+            string MAINMODULEFILENAME = PROCESS.GetMainModuleFileName();
+            if (string.IsNullOrEmpty(MAINMODULEFILENAME)) return null;
+            return Icon.ExtractAssociatedIcon(MAINMODULEFILENAME);
+        } catch { return null; }
+    }
+
+    public static Process GETPARENTPROCESS(this Process PROCESS) {
+        try {
+            int PARENTPID = 0;
+            int PROCESSPID = PROCESS.Id;
+            using (ManagementObject MANAGEMENTOBJECT = new ManagementObject($"win32_process.handle='{PROCESSPID}'")) {
+                MANAGEMENTOBJECT.Get();
+                PARENTPID = Convert.ToInt32(MANAGEMENTOBJECT["ParentProcessId"]);
             }
-            return Process.GetProcessById(parentPid);
-        }
-        catch
-        {
-            return null;
-        }
+
+            return Process.GetProcessById(PARENTPID);
+        } catch { return null; }
     }
 }
 
-public static class IconUtilities
-{
+public static class IconUtilities {
     [DllImport("gdi32.dll", SetLastError = true)]
     private static extern bool DeleteObject(IntPtr hObject);
+    public static ImageSource BITMAPASIMAGESOURCE(this Bitmap ICON) {
+        IntPtr BITMAP = ICON.GetHbitmap();
+        ImageSource IMAGE = Imaging.CreateBitmapSourceFromHBitmap(BITMAP, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        if (!DeleteObject(BITMAP)) throw new Win32Exception();
 
-    public static ImageSource ToImageSource(this Bitmap icon)
-    {
-        IntPtr hBitmap = icon.GetHbitmap();
-
-        ImageSource wpfBitmap = Imaging.CreateBitmapSourceFromHBitmap(
-            hBitmap,
-            IntPtr.Zero,
-            Int32Rect.Empty,
-            BitmapSizeOptions.FromEmptyOptions());
-
-        if (!DeleteObject(hBitmap))
-        {
-            throw new Win32Exception();
-        }
-
-        return wpfBitmap;
+        return IMAGE;
     }
 }
 
-namespace ParoxInjector.Classes
-{
-    internal class ProcessListManager
-    {
-        public static void ShowProcessList(MainWindow mainWindow)
-        {
-            mainWindow.ProcessList.Items.Clear();
-            var ProcessManager = new ProcessListManager();
-            ProcessManager.CreateProcessList(mainWindow);
+namespace ParoxInjector.Classes {
+    internal class ProcessListManager {
+        private static HashSet<string>? PROCESSFILTER;
+        private static readonly string FILTERFILE = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "", "ProcessFilter.txt");
+
+        public static void CLEARPROCESSLIST(MainWindow MAINWINDOW) { MAINWINDOW.PROCESSLIST.Items.Clear(); }
+
+        public static async Task UPDATEPROCESSFILTER() {
+            string? GITHUBFILTER;
+
+            try { GITHUBFILTER = await new HttpClient().GetStringAsync("https://raw.githubusercontent.com/szerveil/ParoxInjector/refs/heads/main/ProcessFilter.txt"); } catch { GITHUBFILTER = null; }
+
+            if(!File.Exists(FILTERFILE)) {
+                try {
+                    await File.WriteAllTextAsync(FILTERFILE, GITHUBFILTER);
+                    DebugFile.INSERT($"[ProcessListManager] Local Process Filter created.");
+                } catch { DebugFile.INSERT($"[ProcessListManager] Local Process Filter creation failed."); }
+            }
+
+            if (File.Exists(FILTERFILE)) {
+                try {
+                    string LOCALFILTER = await File.ReadAllTextAsync(FILTERFILE);
+
+                    if (GITHUBFILTER is not null) {
+                        if (LOCALFILTER == GITHUBFILTER) {
+                            var FILTER = LOCALFILTER.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                            PROCESSFILTER = new HashSet<string>(FILTER, StringComparer.OrdinalIgnoreCase);
+
+                            DebugFile.INSERT($"[ProcessListManager] Local Process Filter is up to date.");
+                            DebugFile.INSERT($"[ProcessListManager] Local Process Filter loaded.");
+                        } else {
+                            await File.WriteAllTextAsync(FILTERFILE, GITHUBFILTER);
+                            DebugFile.INSERT($"[ProcessListManager] Local Process Filter updated.");
+
+                            var FILTER = GITHUBFILTER.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                            PROCESSFILTER = new HashSet<string>(FILTER, StringComparer.OrdinalIgnoreCase);
+
+                            DebugFile.INSERT($"[ProcessListManager] Local Process Filter loaded.");
+                        }
+                    } else {
+                        var FILTER = LOCALFILTER.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                        PROCESSFILTER = new HashSet<string>(FILTER, StringComparer.OrdinalIgnoreCase);
+
+                        DebugFile.INSERT($"[ProcessListManager] Local Process Filter could not be updated.");
+                        if (FILTER.Length > 0) DebugFile.INSERT($"[ProcessListManager] Local Process Filter loaded."); 
+                        else DebugFile.INSERT($"[ProcessListManager] Local Process Filter is empty.");
+
+                        MessageBox.Show("Local Process Filter update failed.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                } catch {}
+            }
         }
 
-        public static void RefreshProcessList(object sender, RoutedEventArgs e, MainWindow mainWindow)
-        {
-            mainWindow.ProcessList.Items.Clear();
-            var ProcessManager = new ProcessListManager();
-            ProcessManager.CreateProcessList(mainWindow);
+        public static async Task SHOW(MainWindow MAINWINDOW) {
+            CLEARPROCESSLIST(MAINWINDOW);
+            if (PROCESSFILTER == null) await UPDATEPROCESSFILTER();
+            await CREATE(MAINWINDOW);
         }
 
-        private async void CreateProcessList(MainWindow mainWindow)
-        {
-            var addedProcessIds = new HashSet<int>();
+        public static async Task REFRESH(object SENDER, RoutedEventArgs ROUTEDEVENTARGS, MainWindow MAINWINDOW) {
+            CLEARPROCESSLIST(MAINWINDOW);
+            if (PROCESSFILTER == null) await UPDATEPROCESSFILTER();
+            await CREATE(MAINWINDOW);
+        }
 
-            await Task.Run(() =>
-            {
-                foreach (var process in Process.GetProcesses())
-                {
-                    if (AddProcessQ(process.ProcessName) || addedProcessIds.Contains(process.Id))
-                    {
-                        continue;
-                    }
+        private static async Task CREATE(MainWindow MAINWINDOW) {
+            var PROCESSLIST = new HashSet<int>();
 
-                    var parentProcess = process.GetParentProcess();
-                    if (parentProcess != null && addedProcessIds.Contains(parentProcess.Id))
-                    {
-                        continue;
-                    }
+            if (PROCESSFILTER == null) await UPDATEPROCESSFILTER();
 
-                    var processInfo = new ProcessInfo
-                    {
-                        ProcessId = process.Id,
-                        ProcessName = process.ProcessName,
-                        WindowTitle = process.MainWindowTitle,
-                        Icon = process.GetIcon() ?? SystemIcons.Application
+            await Task.Run(() => {
+                foreach (var PROCESS in Process.GetProcesses()) {
+                    if (PROCESSFILTER != null && PROCESSFILTER.Contains(PROCESS.ProcessName) || PROCESSLIST.Contains(PROCESS.Id)) continue;
+
+                    var PARENTPROCESS = PROCESS.GETPARENTPROCESS();
+                    if (PARENTPROCESS != null && PROCESSLIST.Contains(PARENTPROCESS.Id)) continue;
+
+                    var PROCESSINFO = new ProcessInfoClass {
+                        PROCESSID = PROCESS.Id,
+                        PROCESSNAME = PROCESS.ProcessName,
+                        WINDOWTITLE = PROCESS.MainWindowTitle,
+                        ICON = PROCESS.GETICON() ?? SystemIcons.Application
                     };
 
-                    Application.Current.Dispatcher.Invoke(() =>
-                    {
-                        var item = new ListBoxItem
-                        {
-                            Content = $"{processInfo.ProcessName} (Process ID: {processInfo.ProcessId})",
-                            Tag = processInfo
+                    Application.Current.Dispatcher.Invoke(() => {
+                        var ITEM = new ListBoxItem {
+                            Content = $"{PROCESSINFO.PROCESSNAME} (Process ID: {PROCESSINFO.PROCESSID})",
+                            Tag = PROCESSINFO
                         };
 
-                        var grid = new Grid();
-                        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(40) });
-                        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                        var GRID = new Grid();
+                        GRID.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(40) });
+                        GRID.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
-                        var bitmap = processInfo.Icon.ToBitmap();
-                        var image = new System.Windows.Controls.Image
-                        {
-                            Source = bitmap.ToImageSource(),
-                            Width = 32,
-                            Height = 32
-                        };
-                        Grid.SetColumn(image, 0);
-                        grid.Children.Add(image);
+                        var BITMAP = PROCESSINFO.ICON.ToBitmap();
+                        var IMAGE = new System.Windows.Controls.Image { Source = BITMAP.BITMAPASIMAGESOURCE(), Width = 32, Height = 32 };
 
-                        var textBlock = new TextBlock
-                        {
-                            Text = $"{processInfo.ProcessName} (Process ID: {processInfo.ProcessId})",
+                        Grid.SetColumn(IMAGE, 0);
+                        GRID.Children.Add(IMAGE);
+
+                        var PID = new TextBlock {
+                            Text = $"{PROCESSINFO.PROCESSNAME} (Process ID: {PROCESSINFO.PROCESSID})",
                             VerticalAlignment = VerticalAlignment.Center,
                             HorizontalAlignment = HorizontalAlignment.Center,
                             FontFamily = new System.Windows.Media.FontFamily("Global Monospace"),
                             Foreground = new SolidColorBrush(Colors.White),
                         };
-                        Grid.SetColumn(textBlock, 1);
-                        grid.Children.Add(textBlock);
 
-                        item.Content = grid;
-                        item.Tag = processInfo;
-                        mainWindow.ProcessList.Items.Add(item);
+                        Grid.SetColumn(PID, 1);
+                        GRID.Children.Add(PID);
 
-                        addedProcessIds.Add(process.Id);
+                        ITEM.Content = GRID;
+                        ITEM.Tag = PROCESSINFO;
+                        MAINWINDOW.PROCESSLIST.Items.Add(ITEM);
+
+                        PROCESSLIST.Add(PROCESS.Id);
                     });
                 }
             });
-        }
-
-        private static bool AddProcessQ(string processName)
-        {
-            string[] excludedProcesses = new string[]
-            {
-        "TextInputHost","ApplicationFrameHost","StartMenuExperienceHost","ShellExperienceHost","conhost","dllhost","sihost","symsrvhost","svchost","vshost","RuntimeBroker","SgrmBroker","UserOOBEBroker","explorer","Idle","SearchUI","System","Secure System","SystemSettings","VcxprojReader","ctfmon","nvsphelper64","SearchApp","SecurityHealthSystray","SecurityHealthService","taskhostw","System","Secure System","Registry","smss","csrss","wininit","services","LsaIso","lsass","winlogon","fontdrvhost","WUDFHost","dwm","dasHost","spoolsrv","warp-svc","pservice","GameManagerService","RtkBtManServ","MsMpEng","MpDefenderCoreService","Memory Compression","audiodg","spoolsv","smartscreen","vcpkgsrv","vctip","MoUsoCoreWorker",
-        "Cloudflare WARP","CrashHandler","Discord","devenv","GameBarPresenceWriter","GameOverlayUI","gamingservices","gamingservicesnet","ParoxInjector","parsecd","msedgewebview2","NisSrv","nvcontainer","NVDisplay.Container","NVIDIA Overlay","Razer Synapse Service","Razer Central","Razer Synapse 3","RazerCentralService","RobloxCrashHandler","RobloxPlayer","RobloxPlayerBeta","RzAppManager","RzBTLEManager","RzChromaConnectServer","RzChromaStreamServer","RzDeviceManager","RzDiagnostic","RzIoTDeviceManager","RzSDKServer","RzSDKService","RzSmartLightingDeviceManager","Spotify","steam","steamservice","steamwebhelper","wallpaper32","Windows10Universal","xgamehelper",
-        "CefSharp.BrowserSubprocess","StandardCollector.Service","ServiceHub.Host.AnyCPU","ServiceHub.DataWarehouseHost","ServiceHub.IndexingService","ServiceHub.ThreadedWaitDialog","ServiceHub.RoslynCodeAnalysisService","ServiceHub.IntellicodeModelService","ServiceHub.TestWindowStoreHost","ServiceHub.SettingsHost","ServiceHub.IdentityHost","ServiceHub.VSDetouredHost","MpCmdRun","Microsoft.ServiceHub.Controller","ServiceHub.Host.dotnet.x64",
-        "PerfWatson2",
-        "WmiPrvSE","CompPkgSrv","MSBuild","VBCSCompiler"
-            };
-
-            return Array.Exists(excludedProcesses, p => p.Equals(processName, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
