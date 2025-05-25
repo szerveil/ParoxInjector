9b4a543f5c11d90f12d0d0b725ce825e3688b9ad
Chris <79816938+szerveil@users.noreply.github.com>
2025-05-24 23:30:54 -0400

    WindowContentManager Class

--- Classes/WindowContentManager.cs
+++ Classes/WindowContentManager.cs
@@ -1,26 +1,10 @@
 using System.Windows.Input;
 using System.Windows;
 
-namespace ParoxInjector.Classes
-{
-    internal class WindowContentManager
-    {
-        public static void TopDrag(object sender, MouseButtonEventArgs e, MainWindow mainWindow)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                mainWindow.DragMove();
-            }
-        }
-
-        public static void Close(object sender, RoutedEventArgs e, MainWindow mainWindow)
-        {
-            mainWindow.Close();
-        }
-
-        public static void Minimize(object sender, RoutedEventArgs e, MainWindow mainWindow)
-        {
-            mainWindow.WindowState = WindowState.Minimized;
-        }
+namespace ParoxInjector.Classes {
+    internal class WindowContentManager {
+        public static void TOPDRAG(object sender, MouseButtonEventArgs MOUSEBUTTONEVENTARGS, MainWindow MAINWINDOW) { if (MOUSEBUTTONEVENTARGS.LeftButton == MouseButtonState.Pressed) MAINWINDOW.DragMove(); }
+        public static void CLOSE(object SENDER, RoutedEventArgs ROUTEDEVENTARGS, MainWindow MAINWINDOW) => MAINWINDOW.Close();
+        public static void MINIMIZE(object SENDER, RoutedEventArgs ROUTEDEVENTARGS, MainWindow MAINWINDOW) => MAINWINDOW.WindowState = WindowState.Minimized;
     }
 }
