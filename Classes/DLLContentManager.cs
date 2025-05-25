25fe0c226ca88e36899afd369533a26969129fa8
Chris <79816938+szerveil@users.noreply.github.com>
2025-05-24 23:28:40 -0400

    DLLContentManager Class

--- Classes/DLLContentManager.cs
+++ Classes/DLLContentManager.cs
@@ -1,223 +1,161 @@
-using System.IO;
+using Microsoft.Win32;
 using Newtonsoft.Json;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
-using System.Windows.Media;
 using System.Windows.Interop;
-using Microsoft.Win32;
-using System.Runtime.InteropServices;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
-public class RecentDLL
-{
-    public string? Name { get; set; }
-    public string? Path { get; set; }
+public class DLL {
+    public string? NAME { get; set; }
+    public string? PATH { get; set; }
 }
 
-public class RecentDLLClass
-{
-    public List<RecentDLL>? RecentDLLs { get; set; }
+public class DLLCOLLECTION {
+    public List<DLL>? DLL_LIST { get; set; }
 }
 
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
 #pragma warning disable CS8604 // Possible null reference argument.
-namespace ParoxInjector.Classes
-{
-    public class DLLContentManager
-    {
+namespace ParoxInjector.Classes {
+    public class DLLContentManager {
         public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
 
         [DllImport("user32.dll", SetLastError = true)]
         public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
-        public static void LoadDLL(object sender, RoutedEventArgs e, MainWindow mainWindow)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog
-            {
-                Filter = "DLL files (*.dll)|*.dll",
-                Title = "Select a DLL file"
-            };
+        public static void LOAD(object sender, RoutedEventArgs e, MainWindow MAINWINDOW) {
+            OpenFileDialog DIALOG = new OpenFileDialog { Filter = "DLL files (*.dll)|*.dll", Title = "Select a DLL file" };
 
-            if (openFileDialog.ShowDialog() == true)
-            {
-                string dllPath = openFileDialog.FileName;
-                mainWindow.DLLPath.Text = dllPath;
+            if (DIALOG.ShowDialog() is true) {
+                string DLLPATH = DIALOG.FileName;
+                MAINWINDOW.DLLPATH.Text = DLLPATH;
 
-                mainWindow.DLLIcon.Source = Imaging.CreateBitmapSourceFromHIcon(
-                    System.Drawing.Icon.ExtractAssociatedIcon(dllPath).Handle,
+                MAINWINDOW.DLLICON.Source = Imaging.CreateBitmapSourceFromHIcon(
+                    System.Drawing.Icon.ExtractAssociatedIcon(DLLPATH).Handle,
                     Int32Rect.Empty,
                     BitmapSizeOptions.FromEmptyOptions()
                 );
-                string dllName = Path.GetFileName(dllPath);
-                mainWindow.DLLName.Text = dllName;
+                string DLLNAME = Path.GetFileName(DLLPATH);
+                MAINWINDOW.DLLNAME.Text = DLLNAME;
 
-                ProcessListManager.ShowProcessList(mainWindow);
-                AddRecent(dllName, dllPath, mainWindow);
+                ProcessListManager.SHOW(MAINWINDOW);
+                ADDRECENT(DLLNAME, DLLPATH, MAINWINDOW);
             }
         }
-        public static void RefreshRecents(MainWindow mainWindow)
-        {
-            try
-            {
-                if (!File.Exists("RecentDLLs.json"))
-                {
-                    var initialData = new RecentDLLClass { RecentDLLs = new List<RecentDLL>() };
-                    string initialJson = JsonConvert.SerializeObject(initialData, Formatting.Indented);
-                    File.WriteAllText("RecentDLLs.json", initialJson);
+
+        public static void REFRESH(MainWindow MAINWINDOW) {
+            try {
+                if (!File.Exists("RecentDLLs.json")) {
+                    var NEWCOLLECTION = new DLLCOLLECTION { DLL_LIST = new List<DLL>() };
+                    string NEWJSON = JsonConvert.SerializeObject(NEWCOLLECTION, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText("RecentDLLs.json", NEWJSON);
                 }
 
-                string json = File.ReadAllText("RecentDLLs.json");
-                RecentDLLClass? data = JsonConvert.DeserializeObject<RecentDLLClass>(json);
+                string JSON = File.ReadAllText("RecentDLLs.json");
+                DLLCOLLECTION? COLLECTION = JsonConvert.DeserializeObject<DLLCOLLECTION>(JSON);
 
-                var itemsToRemove = new List<UIElement>();
-                foreach (UIElement element in mainWindow.RecentDLLContainer.Children)
-                {
-                    if (element is Button || element is StackPanel)
-                    {
-                        itemsToRemove.Add(element);
-                    }
-                }
-                foreach (var item in itemsToRemove)
-                {
-                    mainWindow.RecentDLLContainer.Children.Remove(item);
-                }
+                var REMOVECOLLECTION = new List<UIElement>();
+                foreach (UIElement ELEMENT in MAINWINDOW.RECENTDLLCONTAINER.Children) if (ELEMENT is Button || ELEMENT is StackPanel) REMOVECOLLECTION.Add(ELEMENT);
+                foreach (var ITEM in REMOVECOLLECTION) MAINWINDOW.RECENTDLLCONTAINER.Children.Remove(ITEM);
 
-                if (data != null && data.RecentDLLs != null)
-                {
-                    for (int dllIndex = 0; dllIndex < data.RecentDLLs.Count(); dllIndex++)
-                    {
-                        if(!File.Exists(data.RecentDLLs[dllIndex].Path))
-                        {
-                            DebugFile.Insert($"[DLLContentManager] \"{data.RecentDLLs[dllIndex].Name}\" could not be found at {data.RecentDLLs[dllIndex].Path}\n[DLLContentManager] Removing \"{data.RecentDLLs[dllIndex].Name}\" from RecentDLLs.json {DateTime.Now}");
-                            data.RecentDLLs.Remove(data.RecentDLLs[dllIndex]);
-                            File.WriteAllText("RecentDLLs.json", JsonConvert.SerializeObject(data, Formatting.Indented));
+                if (COLLECTION is not null && COLLECTION.DLL_LIST is not null) {
+                    for (int i = 0; i < COLLECTION.DLL_LIST.Count(); i++) {
+                        if (!File.Exists(COLLECTION.DLL_LIST[i].PATH)) {
+                            DebugFile.INSERT($"[DLLContentManager] \"{COLLECTION.DLL_LIST[i].NAME}\" could not be found at {COLLECTION.DLL_LIST[i].PATH}\n[DLLContentManager] Removing \"{COLLECTION.DLL_LIST[i].NAME}\" from RecentDLLs.json {DateTime.Now}");
+                            COLLECTION.DLL_LIST.Remove(COLLECTION.DLL_LIST[i]);
+                            File.WriteAllText("RecentDLLs.json", JsonConvert.SerializeObject(COLLECTION, Newtonsoft.Json.Formatting.Indented));
                             continue;
                         }
-                        StackPanel contentPanel = new StackPanel
-                        {
-                            Orientation = Orientation.Horizontal,
-                        };
 
-                        Image dllIcon = new Image
-                        {
-                            Source = Imaging.CreateBitmapSourceFromHIcon(
-                                System.Drawing.Icon.ExtractAssociatedIcon(data.RecentDLLs[dllIndex].Path).Handle,
-                                Int32Rect.Empty,
-                                BitmapSizeOptions.FromEmptyOptions()
-                            ),
+                        StackPanel PANEL = new StackPanel { Orientation = Orientation.Horizontal };
+
+                        Image DLLICON = new Image { 
+                            Source = Imaging.CreateBitmapSourceFromHIcon(System.Drawing.Icon.ExtractAssociatedIcon(COLLECTION.DLL_LIST[i].PATH).Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()),
                             Width = 32,
                             Height = 32,
                         };
 
-                        TextBlock dllName = new TextBlock
-                        {
-                            Text = data.RecentDLLs[dllIndex].Name,
+                        TextBlock DLLNAME = new TextBlock {
+                            Text = COLLECTION.DLL_LIST[i].NAME,
                             FontFamily = new FontFamily("Global Monospace"),
                             Foreground = new SolidColorBrush(Colors.White),
                             VerticalAlignment = VerticalAlignment.Center,
                         };
 
-                        contentPanel.Children.Add(dllIcon);
-                        contentPanel.Children.Add(dllName);
+                        PANEL.Children.Add(DLLICON);
+                        PANEL.Children.Add(DLLNAME);
 
-                        Button dllButton = new Button
-                        {
-                            Content = contentPanel,
+                        Button DLL_LOADBUTTON = new Button {
+                            Content = PANEL,
                             Background = Brushes.Transparent,
                             BorderBrush = Brushes.Transparent,
-                            Tag = data.RecentDLLs[dllIndex].Path,
+                            Tag = COLLECTION.DLL_LIST[i].PATH,
                             HorizontalAlignment = HorizontalAlignment.Stretch,
                         };
-                        dllButton.Click += (s, e) => LoadRecentDLL(s, e, mainWindow);
 
-                        mainWindow.RecentDLLContainer.Children.Add(dllButton);
+                        DLL_LOADBUTTON.Click += (SENDER, ROUTEDEVENTARGS) => LOADRECENT(SENDER, ROUTEDEVENTARGS, MAINWINDOW);
+
+                        MAINWINDOW.RECENTDLLCONTAINER.Children.Add(DLL_LOADBUTTON);
                     }
-                }
-                else
-                {
-                    DebugFile.Insert($"[DLLContentManager] Could not find recent DLLs. {DateTime.Now}");
-                }
-            }
-            catch (Exception ex)
-            {
-                DebugFile.Insert($"[DLLContentManager] Failed to fetch Recent DLLs (WARNING: MED) {DateTime.Now}\n[DLLContentManager] {ex.Message}");
-                MessageBox.Show($"Failed to fetch Recent DLLs\nPress \"OK\" to continue.\n{ex.Message}", "WARNING");
+                } else DebugFile.INSERT($"[DLLContentManager] Could not find recent DLLs. {DateTime.Now}");
+            } catch (Exception ex) {
+                DebugFile.INSERT($"[DLLContentManager] Failed to fetch Recent DLLs (WARNING: MED) {DateTime.Now}\n[DLLContentManager] {ex.Message}");
+                MessageBox.Show($"Failed to fetch Recent DLLs\nPress \"OK\" to continue.\n{ex.Message}", "WARNING", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
 
-        public static void LoadRecentDLL(object sender, RoutedEventArgs e, MainWindow mainWindow)
+        public static void LOADRECENT(object SENDER, RoutedEventArgs ROUTEDEVENTARGS, MainWindow MAINWINDOW)
         {
-            Button? clickedButton = sender as Button;
-            if (clickedButton != null & File.Exists("RecentDLLs.json"))
+            Button? BUTTON = SENDER as Button;
+            if (BUTTON != null & File.Exists("RecentDLLs.json"))
             {
-                string? json = File.ReadAllText("RecentDLLs.json");
-                RecentDLLClass? data = JsonConvert.DeserializeObject<RecentDLLClass>(json) ?? new RecentDLLClass { RecentDLLs = new List<RecentDLL>() };
-                foreach (var dll in data.RecentDLLs)
-                {
-                    if(!File.Exists(dll.Path))
-                    {
-                        RefreshRecents(mainWindow);
-                        return;
-                    }
-                }
+                string? JSON = File.ReadAllText("RecentDLLs.json");
+                DLLCOLLECTION? COLLECTION = JsonConvert.DeserializeObject<DLLCOLLECTION>(JSON) ?? new DLLCOLLECTION { DLL_LIST = new List<DLL>() };
+                foreach (var DLL in COLLECTION.DLL_LIST) if (!File.Exists(DLL.PATH)) { REFRESH(MAINWINDOW); return; }
 
-                string? dllPath = clickedButton.Tag.ToString();
-                mainWindow.DLLPath.Text = dllPath;
+                string? DLLPATH = BUTTON.Tag.ToString();
+                MAINWINDOW.DLLPATH.Text = DLLPATH;
 
-                mainWindow.DLLIcon.Source = Imaging.CreateBitmapSourceFromHIcon(
-                    System.Drawing.Icon.ExtractAssociatedIcon(dllPath).Handle,
+                MAINWINDOW.DLLICON.Source = Imaging.CreateBitmapSourceFromHIcon(
+                    System.Drawing.Icon.ExtractAssociatedIcon(DLLPATH).Handle,
                     Int32Rect.Empty,
                     BitmapSizeOptions.FromEmptyOptions()
                 );
-                mainWindow.DLLName.Text = System.IO.Path.GetFileName(dllPath);
+                MAINWINDOW.DLLNAME.Text = Path.GetFileName(DLLPATH);
 
-                ProcessListManager.ShowProcessList(mainWindow);
+                ProcessListManager.SHOW(MAINWINDOW);
             }
         }
 
-        public static void SaveRecent(RecentDLLClass data, MainWindow mainWindow)
-        {
-            try
-            {
-                string json = JsonConvert.SerializeObject(data, Formatting.Indented);
-                File.WriteAllText("RecentDLLs.json", json);
-                RefreshRecents(mainWindow);
-            }
-            catch (Exception ex)
-            {
-                DebugFile.Insert($"[DLLContentManager] Failed to save Recent DLL {DateTime.Now}\n[DLLContentManager] {ex.Message}");
-                MessageBox.Show("Failed to save Recent DLL\nPress \"OK\" to continue.", "WARNING");
+        public static void SAVERECENT(DLLCOLLECTION COLLECTION, MainWindow MAINWINDOW) {
+            try {
+                string JSON = JsonConvert.SerializeObject(COLLECTION, Formatting.Indented);
+                File.WriteAllText("RecentDLLs.json", JSON);
+                REFRESH(MAINWINDOW);
+            } catch (Exception EXCEPTION) {
+                DebugFile.INSERT($"[DLLContentManager] Failed to save Recent DLL {DateTime.Now}\n[DLLContentManager] {EXCEPTION.Message}");
+                MessageBox.Show("Failed to save DLL\nPress \"OK\" to continue.", "WARNING", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
 
-        public static void AddRecent(string name, string path, MainWindow mainWindow)
-        {
-            try
-            {
-                if (!File.Exists("RecentDLLs.json"))
-                {
-                    var initialData = new RecentDLLClass { RecentDLLs = new List<RecentDLL>() };
-                    string initialJson = JsonConvert.SerializeObject(initialData, Formatting.Indented);
-                    File.WriteAllText("RecentDLLs.json", initialJson);
+        public static void ADDRECENT(string NAME, string PATH, MainWindow MAINWINDOW) {
+            try {
+                if (!File.Exists("RecentDLLs.json")) {
+                    var NEWCOLLECTION = new DLLCOLLECTION { DLL_LIST = new List<DLL>() };
+                    string NEWJSON = JsonConvert.SerializeObject(NEWCOLLECTION, Formatting.Indented);
+                    File.WriteAllText("RecentDLLs.json", NEWJSON);
                 }
 
-                string json = File.ReadAllText("RecentDLLs.json");
-                RecentDLLClass data = JsonConvert.DeserializeObject<RecentDLLClass>(json) ?? new RecentDLLClass { RecentDLLs = new List<RecentDLL>() };
+                string JSON = File.ReadAllText("RecentDLLs.json");
+                DLLCOLLECTION COLLECTION = JsonConvert.DeserializeObject<DLLCOLLECTION>(JSON) ?? new DLLCOLLECTION { DLL_LIST = new List<DLL>() };
 
-                foreach (var dll in data.RecentDLLs)
-                {
-                    if (dll.Path == path)
-                    {
-                        return;
-                    }
-                }
-                data.RecentDLLs.Add(new RecentDLL { Name = name, Path = path });
+                foreach (var DLL in COLLECTION.DLL_LIST) if (DLL.PATH == PATH) return;
+                COLLECTION.DLL_LIST.Add(new DLL { NAME = NAME, PATH = PATH });
 
-                SaveRecent(data, mainWindow);
-            }
-            catch (Exception ex)
-            {
-                DebugFile.Insert($"[DLLContentManager] Failed to write recent DLL \"{name}\" to RecentDLLs.json {DateTime.Now}\n[DLLContentManager] {ex.Message}");
-            }
+                SAVERECENT(COLLECTION, MAINWINDOW);
+            } catch (Exception EXCEPTION) { DebugFile.INSERT($"[DLLContentManager] Failed to write recent DLL \"{NAME}\" to RecentDLLs.json {DateTime.Now}\n[DLLContentManager] {EXCEPTION.Message}"); }
         }
     }
 }
