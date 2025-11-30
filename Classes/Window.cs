fc61c71ef7ccbaac3ced6d2f9c9c8d3eed0c3343
Chris <79816938+szerveil@users.noreply.github.com>
2025-11-30 15:31:14 -0500

    Improved readability (negligible)

--- Classes/Window.cs
+++ Classes/Window.cs
@@ -1,10 +1,8 @@
 using System.Windows.Input;
 using System.Windows;
 
-namespace ParoxInjector.Classes
-{
-    internal class WindowContentManager
-    {
+namespace ParoxInjector.Classes {
+    internal class WindowContentManager {
         public static void TOPDRAG(object sender, MouseButtonEventArgs MOUSEBUTTONEVENTARGS, MainWindow MAINWINDOW) { if (MOUSEBUTTONEVENTARGS.LeftButton == MouseButtonState.Pressed) MAINWINDOW.DragMove(); }
         public static void CLOSE(object SENDER, RoutedEventArgs ROUTEDEVENTARGS, MainWindow MAINWINDOW) => MAINWINDOW.Close();
         public static void MINIMIZE(object SENDER, RoutedEventArgs ROUTEDEVENTARGS, MainWindow MAINWINDOW) => MAINWINDOW.WindowState = WindowState.Minimized;
